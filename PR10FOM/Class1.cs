193f1fee01e8d50c427c9be319b3b3aced45beb6
исп-34 <-34@LAB24-04>
2024-12-16 14:24:32 +0300

    Добавьте файлы проекта.

---
+++ PR10FOM/Class1.cs
@@ -0,0 +1,33 @@
+﻿
+namespace WpfLibrary1
+{
+
+    public static class NumberCounter
+    {
+        public static ainura Count(this IEnumerable<int> numbers)
+        {
+            var result = new ainura();
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 > 0)
+                {
+                    result.nechet++;
+                }
+                else
+                {
+                    result.chet++;
+                }
+            }
+
+            return result;
+        }
+
+        public class ainura
+        {
+            public int chet { get; set; } = 0; // Инициализация
+            public int nechet { get; set; } = 0; // Инициализация
+        }
+    }
+}
+
---
+++ PR10FOM/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfLibrary1.NumberCounter;
+
+namespace PR10FOM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string input = txtInput.Text;
+                string[] numberStrings = input.Split(' ');
+
+                if (!numberStrings.All(s => int.TryParse(s, out _)))
+                {
+                    txtResult.Text = "Неправильно введены значения.";
+                    return;
+                }
+
+                var numbers = numberStrings.Select(int.Parse).Where(n => n >= -100 && n <= 100).ToList();
+                if (numbers.Count == 0)
+                {
+                    txtResult.Text = "Не введены числа.";
+                    return;
+                }
+
+                var result = numbers.Count();
+
+                txtResult.Text = $"Четных: {result.chet}\nНечетных: {result.nechet}";
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = $"ошибка: {ex.Message}";
+            }
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string developer = "Фоминов Артем";
+            int job = 10;
+            string task = "Дан массив в диапазоне [-100;100] найти количество Четных и \r\nНечетных.";
+            MessageBox.Show($"Разработчик: {developer}\nНомер работы: {job}\nЗадание: {task}", "О программе");
+        }
+        
+        private void MenuItem_Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    
+    }
+}
